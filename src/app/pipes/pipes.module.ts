49b2683227b5e58b6248659a933e5e9eed750f32
SPablo2191 <pablosandoval2191@gmail.com>
2023-02-11 14:46:31 -0300

    feat: pipe of columns added

---
+++ src/app/pipes/pipes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ColumnPipePipe } from './column-pipe.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    ColumnPipePipe
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PipesModule { }
