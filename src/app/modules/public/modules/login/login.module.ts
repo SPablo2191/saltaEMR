2d901364643f31732cc7fdb3c3889ef29f7f4310
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-03-13 08:48:51 -0300

    feat: login inputs in process

--- src/app/modules/public/modules/login/login.module.ts
+++ src/app/modules/public/modules/login/login.module.ts
@@ -5,6 +5,8 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { ImportsModule } from 'src/app/modules/imports/imports.module';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     LoginRoutingModule,
     ImportsModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class LoginModule { }
